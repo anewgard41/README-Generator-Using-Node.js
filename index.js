0e1d120fb7f8988636bd3dfab95cd37958d19210
Anders Newgard <anne9707@colorado.edu>
2023-09-21 22:24:23 -0400

    started contributing to the question array for user input

--- index.js
+++ index.js
@@ -8,7 +8,55 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Github",
+        message: "What is your Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your preferred email address or the address associated with your Github user?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project called?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project. What do you wish to create?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "what command should be run to install dependencies?",
+        default: "npm i"
+    },
+    {
+        type: "",
+        name: "",
+        message: "",
+    },
+    {
+        type: "",
+        name: "",
+        message: "",
+    },
+    {
+        type: "",
+        name: "",
+        message: "",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
