9d360de7df5b3eb86283b466af931122586d43d0
Anders Newgard <anne9707@colorado.edu>
2023-09-21 22:12:21 -0400

    import package logic added to index.js file

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require ("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
