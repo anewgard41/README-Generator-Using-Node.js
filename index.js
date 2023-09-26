ac46dd53f8bb92d446ba08450d27b395a4028d8c
Anders Newgard <anne9707@colorado.edu>
2023-09-25 22:18:33 -0400

    another attempt to fix the issues with license images

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const questions = [
         type: "list",
         name: "license",
         message: "What kind of license should your project have?",
-        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        choices: ["MIT", "APACHE2.0", "GPL3.0", "BSD3", "None"]
     },
     {
         type: "input",
